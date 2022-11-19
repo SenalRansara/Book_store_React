59036281b5e40f0220875467da22a247e374160b
SenalRansara <senal.ransara38@gmail.com>
2022-11-19 20:53:22 +0530

    redo the changes

--- model/BookModel.js
+++ model/BookModel.js
@@ -1,30 +1,30 @@
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// const Schema = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-// const bookSchema = new Schema({
+const bookSchema = new Schema({
 
-//     id: {
-//         type: String,
-//         required: true
-//     },
+    id: {
+        type: String,
+        required: true
+    },
 
-//     title: {
-//         type: String,
-//         required: true
-//     },
+    title: {
+        type: String,
+        required: true
+    },
 
-//     author: {
-//         type: String,
-//         required: true
-//     },
+    author: {
+        type: String,
+        required: true
+    },
 
-//     cost: {
-//         type: String,
-//         required: true,
-//     }
-// })
+    cost: {
+        type: String,
+        required: true,
+    }
+})
 
-// const Book = mongoose.model("Book", bookSchema);
+const Book = mongoose.model("Book", bookSchema);
 
-// module.exports = Book;
+module.exports = Book;
