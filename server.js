25b0ffa594fff7e72b78419f430cff0f5f89de9a
Senal Ransara <69238922+SenalRansara@users.noreply.github.com>
2022-11-19 17:21:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
 
-require ("dotenv").config();
+// require ("dotenv").config();
 
-const app = express();
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+// const app = express();
+// app.use(express.urlencoded({ extended: false }))
+// app.use(express.json())
 
-app.use(cors())
+// app.use(cors())
 
-const PORT = process.env.PORT || 8080;
-const URL = process.env.MONGODB_URL;
+// const PORT = process.env.PORT || 8080;
+// const URL = process.env.MONGODB_URL;
 
-//Book management - Senal
-const Book = require("./controller/BookController");
-app.use("/api", Book);
-
-
+// //Book management - Senal
+// const Book = require("./controller/BookController");
+// app.use("/api", Book);
 
 
-mongoose.connect(URL).then(()=> {
-    console.log('DB Connected Successfully');
-})
-.catch((err) => console.log('DB Connection Error',err));
-
-app.listen(PORT,()=>{
-    console.log(`App is running on ${PORT}`);
-});
 
 
-app.get('/', (req, res) => {
-  res.send('<h1>Book store App Backend is Successfully connected </h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
-})
+// mongoose.connect(URL).then(()=> {
+//     console.log('DB Connected Successfully');
+// })
+// .catch((err) => console.log('DB Connection Error',err));
+
+// app.listen(PORT,()=>{
+//     console.log(`App is running on ${PORT}`);
+// });
+
+
+// app.get('/', (req, res) => {
+//   res.send('<h1>Book store App Backend is Successfully connected </h1> <h4>Message: Success</h4> <p>Version: 1.0.0</p>');
+// })
 
 
 
